a9c089b3497caf65fc650c61331d807be11bfe02
mineachem <minea.chem@gmail.com>
2016-05-30 14:33:46 -0700

    hello

--- src/mainAppPackage/MainApp.java
+++ src/mainAppPackage/MainApp.java
@@ -3,12 +3,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 public class MainApp  {
     public static void main(String[] arg) throws ClassNotFoundException, FileNotFoundException, InterruptedException, IOException{		
-//        TestWrite1000 wr=new TestWrite1000();
-//        wr.write1000();
+        TestWrite1000 wr=new TestWrite1000();
+        wr.write1000();
     
         
         mainMenu main=new mainMenu();
         main.displayMenu();
+        //Test display
         
         
     }
